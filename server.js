ad8bb01031d4d523368c5d376382cc0573ce253d
helloprof <suluxan.seneca@gmail.com>
2023-11-08 11:48:06 -0500

    get route for serving form, post route for capture form data using multer, setting up for cloudinary functionality

--- server.js
+++ server.js
@@ -3,11 +3,28 @@ const linkUpService = require("./linkUpService")
 
 const express = require("express")
 const app = express()
+
+const multer = require('multer');
+const cloudinary = require('cloudinary').v2
+const streamifier = require('streamifier')
+
 const HTTP_PORT = 8080
 
 app.set('view engine', 'ejs')
 
 app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }));
+
+// const upload = multer();
+
+const storage = multer.diskStorage({
+    destination: 'public/images/',
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + path.extname(file.originalname));
+    },
+  });
+  
+const upload = multer({ storage: storage });
 
 app.get("/", (req, res) => {
     // req.get("User-Agent")
@@ -67,6 +84,54 @@ app.get("/profiles/position/:id", (req, res) => {
     }).catch((err) => {
         res.send(err)
     })
+})
+
+app.get('/profiles/new', (req, res) => {
+    res.render('addProfiles')
+})
+
+app.post('/profiles/new', upload.single('image'), function (req, res, next) {
+    // if (req.file) {
+    // API from: https://cloudinary.com/blog/node_js_file_upload_to_a_local_server_or_to_the_cloud
+    // let streamUpload = (req) => {
+    //     return new Promise((resolve, reject) => {
+    //         let stream = cloudinary.uploader.upload_stream(
+    //           (error, result) => {
+    //             if (result) {
+    //               resolve(result)
+    //             } else {
+    //               reject(error)
+    //             }
+    //           }
+    //         );
+
+    //       streamifier.createReadStream(req.file.buffer).pipe(stream)
+    //     })
+    // }
+
+    // async function upload(req) {
+    //     let result = await streamUpload(req)
+    //     console.log(result)
+    // }
+
+    // upload(req).then((uploaded) => {
+    //     console.log(uploaded.url)
+    //     // req.body.image = uploaded.url 
+    //     processUpload(uploaded.url)
+    // })
+    // } else {
+    //     processUpload("")
+    // }
+
+    // function processUpload(uploadURL) {
+    //     req.body.image = uploaded.url 
+
+
+    // }
+
+    
+    console.log(req.file)
+    res.send(req.body)
 })
 
 app.get("/profiles/:id", (req, res) => {
